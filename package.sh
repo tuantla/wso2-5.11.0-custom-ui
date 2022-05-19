a5ab1751fec07de90025e441a95677402bac2b60
TuanVu <robin.github@gmail.com>
2022-05-19 16:30:28 +0800

    Add remote configuration

--- package.sh
+++ package.sh
@@ -2,6 +2,13 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 set -e
 
+#mvn $MAVEN_CONFIG dependency:get \
+#    -DrepoUrl=https://nexus.101digital.io/repository/all \
+#    -DrepositoryId=101digital \
+#    -Dartifact=org.wso2.extensions:config-client:1.0.0-SNAPSHOT:jar \
+#    -Dtransitive=false \
+#    -Ddest=emailotpauthenticationendpoint/WEB-INF/lib/config-client-1.0.0-SNAPSHOT.jar
+
 zip -r -qq authenticationendpoint.zip authenticationendpoint
 zip -r -qq emailotpauthenticationendpoint.zip emailotpauthenticationendpoint
  
